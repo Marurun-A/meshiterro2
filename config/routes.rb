2f1b162c6c26aff1e281dfda2df260bff8fef06b
EC2 Default User <ec2-user@ip-172-31-39-82.ap-northeast-1.compute.internal>
2023-12-05 05:57:42 +0000

    [Finish] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  
-  resources :post_images, only: [:new, :index, :show]
+
+  resources :post_images, only: [:new, :create, :index, :show]
   # get 'postlmages/new'
   # get 'postlmages/index'
   # get 'postlmages/show'
-  
+
   get 'homes/about', to: 'homes#about', as: 'about'
   #
 end
--- test/controllers/post_images_controller_test.rb
+++ test/controllers/post_images_controller_test.rb
@@ -1,18 +1,18 @@
 require "test_helper"
 
-class PostlmagesControllerTest < ActionDispatch::IntegrationTest
+class PostImagesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get postlmages_new_url
+    get post_images_new_url
     assert_response :success
   end
 
   test "should get index" do
-    get postlmages_index_url
+    get post_images_index_url
     assert_response :success
   end
 
   test "should get show" do
-    get postlmages_show_url
+    get post_images_show_url
     assert_response :success
   end
 end
